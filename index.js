8615339f9dea1977195a2bba61a91d35880bafb7
RedMikePumpkin <michael@malinov.com>
2020-06-24 18:55:25 -0700

    synchronize with index.min.js

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 (()=>{
   var a=(n,p,m)=>{
     if(!m)m=0;
-    try {
-    new Array(p).fill(0).forEach(i=>{n=n.parentNode});
-    new Array(m).fill(0).forEach(i=>{n=n.childNodes[0]});
-    } catch {
-      return null;
+    try{
+      new Array(p).fill(0).forEach(i=>{n=n.parentNode});
+      new Array(m).fill(0).forEach(i=>{n=n.childNodes[0]})
+    }catch{
+      return null
     }
     return n
   };
   Array.from(document.querySelectorAll('div[data-testid="primaryColumn"] .css-901oao')).filter(i=>i.innerHTML==='Promoted').forEach((b)=>{
     if(b){
-      var c=a(b,10,0),
-      d=a(b,4,5);
+      var c=a(b,10,0),d=a(b,4,5);
       if(d)d=d.href;
-      if(!d||!c){
-        return;
-      }
+      if(!d||!c)return;
       var e=window.open(d,"_blank","width=500,height=500");
       c.remove();
       setTimeout(()=>{
         var z=e.document.querySelector('div[role="button"][aria-expanded="false"][aria-label="More"]');
-        if(z)z.click();
+        if(!z)return;
+        z.click();
         setTimeout(()=>{
           e.document.querySelector('div[data-testid="mute"]').click()
         },100);
@@ -30,5 +28,5 @@
         },200)
       },5000)
     }
-  });
+  })
 })()
